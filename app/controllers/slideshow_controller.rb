39c1b0ce2d9f384b306190e429512c660b1a3bd6
banjun <banjun@gmail.com>
2013-06-15 18:39:14 +0900

    add title,description,tags to slides

--- app/controllers/slideshow_controller.rb
+++ app/controllers/slideshow_controller.rb
@@ -4,14 +4,24 @@ class SlideshowController < ApplicationController
   layout false
   def index
     sizes = %w(url_l url_c url_z url_n).freeze
+    extras = (sizes + %w(description geo tags)).join(',').freeze
     @photos = Keyword.where(user_id: current_user).map{|keyword|
-      flickr.photos.search(text: keyword.keyword, extras: sizes.join(',')).to_a
+      flickr.photos.search(text: keyword.keyword, extras: extras).to_a
     }.flatten.shuffle[0,10].map{|photo|
       begin
         # 2-nd lagrgest photo
         # (1st largeest is original, but it is so big)
+        p photo
         source = sizes.map{|s| photo[s]}.compact.first
         source ||= flickr.photos.getSizes(photo_id: photo['id'])[-2]['source']
+        {
+          :source => source,
+          :title => photo['title'],
+          :tags => photo['tags'].split,
+          :description => photo['description'],
+          :latitude => photo['latitude'],
+          :longitude => photo['longitude'],
+        }
       rescue
         []
       end
