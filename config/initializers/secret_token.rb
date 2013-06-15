483d5b4eb6abce2c8944552c9965c65f84e50e13
mzp <mzpppp@gmail.com>
2013-06-15 11:26:34 +0900

    rails new mirai-stream -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiraiStream::Application.config.secret_token = '5be3cf640653642af1b6ab4b7d551320602536fa2f857df5dae343c063893539a511f9cb9a3f7fbaf2d99947cb27b7e26572ce75009a8a46158193c6ee7b94ac'
