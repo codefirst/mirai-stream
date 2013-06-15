f74c86bc07be2212d3f2bfb53984ba2722b17f9d
banjun <banjun@gmail.com>
2013-06-15 17:55:06 +0900

    reduce getSizes API call

--- app/controllers/slideshow_controller.rb
+++ app/controllers/slideshow_controller.rb
@@ -3,14 +3,15 @@ require 'flickraw'
 class SlideshowController < ApplicationController
   layout false
   def index
+    sizes = %w(url_l url_c url_z url_n).freeze
     @photos = Keyword.where(user_id: current_user).map{|keyword|
-      flickr.photos.search(text: keyword.keyword).to_a
+      flickr.photos.search(text: keyword.keyword, extras: sizes.join(',')).to_a
     }.flatten.shuffle[0,10].map{|photo|
       begin
         # 2-nd lagrgest photo
         # (1st largeest is original, but it is so big)
-        x = flickr.photos.getSizes(photo_id: photo['id'])[-2]
-        x['source']
+        source = sizes.map{|s| photo[s]}.compact.first
+        source ||= flickr.photos.getSizes(photo_id: photo['id'])[-2]['source']
       rescue
         []
       end
